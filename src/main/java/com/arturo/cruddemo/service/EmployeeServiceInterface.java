06b686e9237b9f31633cd8e4806c23d3ff37a964
Artur Yusupov <aptypo@li.ru>
2022-05-13 16:24:50 +0300

    Revert "Initial commit"
    This reverts commit e9cfa877f2c1b95b8da2739d9cd1fc73196906f1.

--- src/main/java/com/arturo/cruddemo/service/EmployeeServiceInterface.java
+++
@@ -1,7 +0,0 @@
-package com.arturo.cruddemo.service;
-
-import com.arturo.cruddemo.entity.Employee;
-
-public interface EmployeeServiceInterface {
-    public Employee addEmployee(Employee employee);
-}
